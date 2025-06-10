0cdc3250266843333ffcb856b8f7ce3b913751fd
unknown <lb@silverliningresearch.com>
2025-06-10 22:59:05 +0000

    update new data

--- Departures/statistics.js
+++ Departures/statistics.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-02",
         "quota_id": "CZ-CAN",
         "Number of interviews": 5,
-        "download_time": "11-06-2025 06:27:19"
+        "download_time": "11-06-2025 06:58:56"
     },
     {
         "InterviewDate": "2024-10-02",
