c64a19723c6d07e44580bd0144d08061917966a8
unknown <lb@silverliningresearch.com>
2026-06-11 01:37:42 +0000

    update new data

--- Departures/statistics.js
+++ Departures/statistics.js
@@ -3,7 +3,7 @@ let interview_statistics_asq = `[
         "InterviewDate": "2024-10-02",
         "quota_id": "CZ-CAN",
         "Number of interviews": 5,
-        "download_time": "11-06-2026 09:05:21"
+        "download_time": "11-06-2026 09:37:32"
     },
     {
         "InterviewDate": "2024-10-02",
@@ -3953,6 +3953,11 @@ let interview_statistics_asq = `[
     {
         "InterviewDate": "2026-06-11",
         "quota_id": "TR-BKK",
+        "Number of interviews": 5
+    },
+    {
+        "InterviewDate": "2026-06-11",
+        "quota_id": "TR-DPS",
         "Number of interviews": 5
     }
 ] `;
